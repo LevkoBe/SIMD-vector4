388ce3566740765d522aef9658d60df3fae9649d
LevkoBe <lbeniakh@kse.org.ua>
2025-04-10 18:52:47 +0300

    refact: optimization

--- SIMD-vector4/SIMD-vector4.cpp
+++ SIMD-vector4/SIMD-vector4.cpp
@@ -2,67 +2,59 @@
 #include <xmmintrin.h>
 #include <cmath>
 
+// two decisions I made:
+// 1. in-place operations for better efficiency and chaining available (and because it's implied in the task)
+// 2. internal data as __m128 for better efficiency (although the opposite is implied in the task)
+
 struct vector4 {
 private:
-	float _x, _y, _z, _w;
-public:
-	vector4() : _x(0), _y(0), _z(0), _w(1.0f) {}
-	vector4(float x, float y, float z, float w) : _x(x), _y(y), _z(z), _w(w) {}
+	__m128 v;
 
-	float x() const { return _x; }
-	float y() const { return _y; }
-	float z() const { return _z; }
-	float w() const { return _w; }
+	float get(size_t index) const {
+		alignas(16) float arr[4];
+		_mm_store_ps(arr, v);
+		return arr[index];
+	}
+
+public:
+	vector4(float x, float y, float z) : v(_mm_set_ps(0.0f, z, y, x)) {}
+	vector4(float x, float y, float z, float w) : v(_mm_set_ps(w, z, y, x)) {}
+
+	float x() const { return _mm_cvtss_f32(v); }
+	float y() const { return get(1); }
+	float z() const { return get(2); }
+	float w() const { return get(3); }
 
 	vector4& add(const vector4& other) {
-		__m128 ths = _mm_set_ps(_w, _z, _y, _x);
-		__m128 tht = _mm_set_ps(other._w, other._z, other._y, other._x);
-		__m128 res = _mm_add_ps(ths, tht);
-		store(res);
+		v = _mm_add_ps(v, other.v);
 		return *this;
 	}
 	vector4& add(float x, float y, float z) {
-		__m128 ths = _mm_set_ps(_w, _z, _y, _x);
 		__m128 tht = _mm_set_ps(0.0f, z, y, x);
-		__m128 res = _mm_add_ps(ths, tht);
-		store(res);
+		v = _mm_add_ps(v, tht);
 		return *this;
 	}
 
 	vector4& sub(const vector4& other) {
-		__m128 ths = _mm_set_ps(_w, _z, _y, _x);
-		__m128 tht = _mm_set_ps(other._w, other._z, other._y, other._x);
-		__m128 res = _mm_sub_ps(ths, tht);
-		store(res);
+		v = _mm_sub_ps(v, other.v);
 		return *this;
 	}
 	vector4& sub(float x, float y, float z) {
-		__m128 ths = _mm_set_ps(_w, _z, _y, _x);
 		__m128 tht = _mm_set_ps(0.0f, z, y, x);
-		__m128 res = _mm_sub_ps(ths, tht);
-		store(res);
+		v = _mm_sub_ps(v, tht);
 		return *this;
 	}
 
 	static friend std::ostream& operator<<(std::ostream& os, const vector4& v) {
-		os << "vector4(" << v._x << ", " << v._y << ", " << v._z << ", " << v._w << ")";
+		os << "(" << v.x() << ", " << v.y() << ", " << v.z() << ", " << v.w() << ")";
 		return os;
-	}
-
-	void store(const __m128& v) {
-		alignas(16) float res[4];
-		_mm_store_ps(res, v);
-		_x = res[0];
-		_y = res[1];
-		_z = res[2];
-		_w = res[3];
 	}
 };
 
 int main()
 {
-	vector4 v1(1.0f, 2.0f, 3.0f, 4.0f);
-	vector4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+	vector4 v1(1.0f, 2.0f, 3.0f, 0.0f);
+	vector4 v2(5.0f, 6.0f, 7.0f, 0.0f);
 
 	std::cout << "v1: " << v1 << std::endl;
 	std::cout << "v2: " << v2 << std::endl;
